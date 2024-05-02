89feed96691b887692d31f62ce43b4adfca70315
naveen7addala <Naveen@789>
2024-05-02 13:47:07 +0530

    another push

--- src/main/java/org/example/TestClass.java
+++ src/main/java/org/example/TestClass.java
@@ -2,6 +2,6 @@ package org.example;
 
 public class TestClass {
     public static void main(String[] args) {
-        System.out.println("NEw class");
+        System.out.println("New branch class");
     }
 }
