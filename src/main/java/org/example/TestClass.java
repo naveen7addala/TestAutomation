606bdf0ebd5c7e475747a3f71c09823d5e871b98
naveen7addala <Naveen@789>
2024-05-02 13:29:10 +0530

    another push

--- src/main/java/org/example/TestClass.java
+++ src/main/java/org/example/TestClass.java
@@ -2,6 +2,6 @@ package org.example;
 
 public class TestClass {
     public static void main(String[] args) {
-        System.out.println("New code from Naveen branch");
+        System.out.println("added code from Naveen branch");
     }
 }
