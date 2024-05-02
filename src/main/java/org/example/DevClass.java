03d4b3a39b05ad3b7ac06ae2362a2644244a517f
naveen7addala <Naveen@789>
2024-05-02 13:15:38 +0530

    Second push

--- src/main/java/org/example/DevClass.java
+++ src/main/java/org/example/DevClass.java
@@ -4,5 +4,6 @@ public class DevClass {
 
     public static void main(String[] args) {
         System.out.println("Hello World, this is DevClass");
+        System.out.println("Added new line");
     }
 }
